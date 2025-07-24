7fcd7f54e4f650a67252de6e8420182c75f2580d
Hugo Munoz Gris <hmunoz-g@cbr5s1.42barcelona.com>
2025-07-24 18:41:13 +0200

    I will never stop pushing before makefcleaning, it seems

--- .dep/include/glad/src/glad.d
+++
@@ -1,4 +0,0 @@
-.obj/include/glad/src/glad.o: include/glad/src/glad.c include/glad/glad.h \
- include/KHR/khrplatform.h
-include/glad/glad.h:
-include/KHR/khrplatform.h:
--- .dep/src/main.d
+++
@@ -1,6 +0,0 @@
-.obj/src/main.o: src/main.cpp include/glad/glad.h \
-  include/KHR/khrplatform.h
-
-include/glad/glad.h:
-
-include/KHR/khrplatform.h:
